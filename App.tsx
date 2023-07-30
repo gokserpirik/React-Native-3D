df03ebe0ce4bea5971750e01a0f3727c8ea0d1c2
Gökser <48176173+gokserpirik@users.noreply.github.com>
2023-07-30 11:14:10 +0300

    firstTry

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber/native'
+import React, { Suspense} from 'react';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
-export default function App() {
+
+
+
+
+function Phone(){
+
+  const myMesh = React.useRef();
+  /* one circle on start on x axis */
+  useFrame(({ clock }) => {
+   if(myMesh.current){
+    myMesh.current.rotation.y += 0.01;
+    
+   }
+
+  })
+
+
+
+  const phone = useLoader(OBJLoader, require('./phone/ip.obj'));
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <mesh ref={myMesh} rotation={[0,1,0]} > 
+      <primitive object={phone} scale={0.01}  />
+ 
+    </mesh>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+export default function App() {
+
+
+  return (
+    <Canvas >
+
+    <pointLight position={[10, 10, 10]} />
+
+    <Suspense fallback={null}>
+    <Phone />
+
+    </Suspense>
+    <mesh position={[0,-0.2,-0.1]} >
+
+      <cylinderGeometry args={[0.9,1.2,0.5,64]} />
+      <meshStandardMaterial  />
+    </mesh>
+
+    
+   
+  </Canvas>
+  );
+}
